cbd5eb8c4764f9c72a2e7b644c0f81f177ac311f
Thomas Gallagher <gallagth@gmail.com>
2019-06-28 14:36:19 +0200

    Task 28-31

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -9,6 +9,8 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
 }
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+operator fun MyDate.plus(time: TimeInterval) = this.addTimeIntervals(time, 1)
+operator fun MyDate.plus(time: RepeatedTimeInterval) = this.addTimeIntervals(time.ti, time.n)
 
 enum class TimeInterval {
     DAY,
@@ -16,13 +18,25 @@ enum class TimeInterval {
     YEAR
 }
 
+class RepeatedTimeInterval(val ti: TimeInterval, val n: Int)
+
+operator fun TimeInterval.times(n: Int) = RepeatedTimeInterval(this, n)
+
 class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
     operator fun contains(date: MyDate) = start <= date && date <= endInclusive
     override fun iterator() = DateIterator(this)
 }
 
-class DateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+class DateIterator(private val dateRange: DateRange) : Iterator<MyDate> {
     var current: MyDate = dateRange.start
-    override fun hasNext() = current.nextDay() < this.dateRange.endInclusive
-    override fun next() = current.nextDay()
+
+    override fun hasNext() :Boolean {
+        return current <= this.dateRange.endInclusive
+    }
+
+    override fun next() : MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
 }
